046e321eb0b0b49e265d01048c344f2f104291fc
vsn80395 <vsn80395@gmail.com>
2022-06-25 12:35:35 +0800

    加入專案檔案。

---
+++ WebApplication2Tests/HomeControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void EditTest()
+        {
+            //Assert.Fail();
+        }
+    }
+}
